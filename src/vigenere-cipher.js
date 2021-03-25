e735dcdcc84a6f1c5f60eaadb44edecffb608583
MariaCherkez <33730446+MariaCherkez@users.noreply.github.com>
2021-03-25 23:32:40 +0200

    init: start vigenere-cipher-task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,50 @@
 const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+	constructor(isStraight = true) {
+		this.isStraight = isStraight; 	
   }
-}
+  
+  encrypt(message, key) {
+	if (!message || !key)
+		throw new Error('undefined'); 
+	
+	const arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];	 
+	let k = -1;
+	
+	key = "".padStart(message.split('').filter(e => arr_EN.indexOf(e.toUpperCase()) != -1).length, key).split(''); 	
+	let result = message.split('').map(function(e,i) {
+		if (arr_EN.indexOf(e.toUpperCase()) === -1)
+			return e;
+		else	
+			return arr_EN[(arr_EN.indexOf(e.toUpperCase()) + arr_EN.indexOf(key[++k].toUpperCase()))% 26];
+	});		
+	
+	return this.isStraight ? result.join('') : result.reverse().join('');  
+  }   
+  
+   decrypt(message, key) {
+	if (!message || !key)
+		throw new Error('undefined');
+	
+	const arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];	
+	let k = -1;
+	
+	key = "".padStart(message.split('').filter(e => arr_EN.indexOf(e.toUpperCase()) != -1).length, key).split('');  
+	
+	let result = message.split('').map( function(e,i) {
+		if (arr_EN.indexOf(e.toUpperCase()) === -1)
+			return e;
+		else {
+			let index = (arr_EN.indexOf(e.toUpperCase()) - arr_EN.indexOf(key[++k].toUpperCase()))% 26;
+			if (index >= 0)
+				return arr_EN[index];
+			else {
+				return arr_EN[arr_EN.length + index]; 
+			} 
+		} 
+	}); 
+	return this.isStraight ? result.join('') : result.reverse().join('');    
+  }
+} 
 
-module.exports = VigenereCipheringMachine;
+module.exports = VigenereCipheringMachine; 

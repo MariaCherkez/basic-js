c79150dcbe11fe86d82a61e1dc366cc08ef034ce
MariaCherkez <33730446+MariaCherkez@users.noreply.github.com>
2021-03-25 23:32:07 +0200

    init: start simple-chain-task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
+const chainMaker = { 
+  chain: [],	
+  
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+	return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {    
+  if (value !== undefined)
+	this.chain.push('( ' + String(value) + ' )'); 
+  else 
+	this.chain.push('(  )');  
+	
+  return this;
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {   
+  if (typeof position != 'number' || String(position).indexOf(".") != -1 || this.chain[position-1] == undefined || position < 1) { 
+	this.chain = [];
+	throw new Error;
+  } 
+   this.chain.splice(position-1, 1); 
+   return this;
   },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  reverseChain() {  
+   this.chain.reverse();
+   return this;
   },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  finishChain() {  
+	let res = this.chain.join('~~'); 
+	this.chain = []; 
+	return res; 
   }
 };
 
